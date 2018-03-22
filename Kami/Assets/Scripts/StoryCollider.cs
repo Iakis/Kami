e831773203ef981c7926a5230120ef0bfcb4a071
Meeral Qureshi <meeralqureshi@hotmail.com>
2018-03-22 02:46:24 -0400

    Added basics of story

---
+++ Kami/Assets/Scripts/StoryCollider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryCollider : MonoBehaviour {
+
+	bool storyActivated;
+	public Text story;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("Collided");
+		if (col.CompareTag ("Player")) 
+		{
+			Debug.Log ("Collided");
+			Debug.Log (this.gameObject.name);
+			// Check if story activated
+			if(!storyActivated){
+				// Call story point text depending on name
+				if(this.gameObject.name == "StoryCollider1"){
+					Debug.Log ("Fading Text");
+					StartCoroutine(story.GetComponent<StoryText> ().FadeText ("She's nearby, I can feel it"));
+					storyActivated = true;
+				}
+				if(this.gameObject.name == "StoryCollider2"){
+					StartCoroutine(story.GetComponent<StoryText> ().FadeText ("I see a house over there"));
+					storyActivated = true;
+				}
+				if(this.gameObject.name == "StoryCollider3"){
+					StartCoroutine(story.GetComponent<StoryText> ().FadeText ("Is that her over there?"));
+					storyActivated = true;
+				}
+				if(this.gameObject.name == "StoryCollider4"){
+					StartCoroutine(story.GetComponent<StoryText> ().FadeText ("I'll get you out of here, I promise"));
+					storyActivated = true;
+				}
+			}
+		}
+	}
+}
---
+++ Kami/Assets/Scripts/StoryText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryText : MonoBehaviour {
+
+	private float fadeTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+		//Story.color = new Color (255, 255, 255, 0);
+		//Debug.Log (Story.color.a);
+		StartCoroutine(FadeText("I have to find her..."));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+ 
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+	public IEnumerator FadeText(string storyText){
+		GetComponent<Text>().text = storyText;
+		Debug.Log("Fading story text");
+		StartCoroutine(FadeTextToFullAlpha(2f, GetComponent<Text>()));
+		yield return new WaitForSeconds(3);
+		StartCoroutine (FadeTextToZeroAlpha(2f, GetComponent<Text>()));
+	}
+}
