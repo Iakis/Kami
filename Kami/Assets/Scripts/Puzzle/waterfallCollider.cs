8b86fea33293f7ed3bb71076c894a774413c4340
lorindalast <lorindalast@gmail.com>
2018-04-01 22:45:26 -0400

    added waterfall collider

---
+++ Kami/Assets/Scripts/Puzzle/waterfallCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfallCollider : MonoBehaviour {
+
+    public bool isBroken;
+    public ParticleSystem left, right;
+
+	// Use this for initialization
+	void Start () {
+        isBroken = false;
+        left = GameObject.Find("left").GetComponent<ParticleSystem>();
+        right = GameObject.Find("right").GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (left == null && right == null)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
